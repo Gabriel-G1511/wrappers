161b049e634a34ba5455eb5cf57f36532db86dd7
Gabriel-G1511 <gab.gon1511@gmail.com>
2026-04-10 08:39:22 -0300

    nova atualização dos wrappers

--- wrappers.java
+++ wrappers.java
@@ -2,16 +2,11 @@ public class wrappers {
     public static void main(String[] args) {
 
 
-        int anos = 26;
-        System.out.println("olá, meu nome é gabriel e tenho " + anos + " anos");
+                int anos = 26;
+                System.out.println("olá, meu nome é gabriel e tenho " + anos + " anos");
 
-        Integer anosWrapper = anos; // autoboxing
-        anosWrapper = 27;
+                Integer anosWrapper = Integer.valueOf(anos); // conversão explícita
 
-        int anosDeNovo = anosWrapper; // unboxing
-
-        System.out.println("agora irei fazer " + anosDeNovo + " anos em novembro");
-
-    }
-}
-//
+                System.out.println("valor wrapper: " + anosWrapper);
+            }
+        }
